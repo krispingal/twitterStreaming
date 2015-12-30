c2e433cf4835a1a27f128fd3f19b02a4f702b34e
Krishna Pingal <krishna.pingal@gmail.com>
2015-12-30 18:02:33 +0530

    Remove init

--- streamTweets.go
+++ streamTweets.go
@@ -4,26 +4,9 @@ import (
 	"os"
 	"fmt"
 	"net/url"
-	"log"
 	
 	"github.com/ChimeraCoder/anaconda"
 )
-
-func init() {
-	consumer_key := os.Getenv("TWITTER_CONSUMER_KEY")
-	consumer_secret := os.Getenv("TWITTER_CONSUMER_SECRET")
-	access_token := os.Getenv("TWITTER_ACCESS_TOKEN")
-	access_token_secret := os.Getenv("TWITTER_ACCESS_TOKEN_SECRET")
-	
-	if consumer_key == "" || consumer_secret == "" {
-		log.Fatalln("consumer tokens left blank")
-	}
-	
-	if access_token == "" || access_token_secret == "" {
-		log.Fatalln("access tokens left blank")
-	}
-	fmt.Println("Consumer tokens and keys available in environment \n Initiating connection with twitter api")
-}
 
 func main() {
 	consumer_key := os.Getenv("TWITTER_CONSUMER_KEY")
